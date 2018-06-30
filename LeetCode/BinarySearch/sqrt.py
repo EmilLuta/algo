80955be761c9e9c3645fc0eeddd0e23f7dae7fca
Devil <virgil.luta@gmail.com>
2018-06-30 18:39:24 +0300

    Binary Search - SQRT implementation

---
+++ LeetCode/BinarySearch/sqrt.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        left, right = 0, x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid ** 2 == x:
+                return mid
+            elif mid ** 2 > x:
+                right = mid - 1
+            else:
+                left = mid + 1
+            mid = (left + right) // 2
+        return mid
+
+
+print(Solution().mySqrt(8))
