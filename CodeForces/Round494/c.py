2c584a76ec7de48e5bbb76372239c7fbde89f907
Devil <virgil.luta@gmail.com>
2018-07-04 20:36:15 +0300

    Fix problem C for Round494

--- CodeForces/Round494/c.py
+++ CodeForces/Round494/c.py
@@ -2,21 +2,14 @@ def go():
     n, k = [int(i) for i in input().split(' ')]
     a = [int(i) for i in input().split(' ')]
     maxx = float('-inf')
-    if k == 1:
-        return float(max(a))
-    keep = {}
-    for i in range(n):
+    for i in range(n - k + 1):
+        s = sum(a[i:i + k - 1])
         for j in range(i + k - 1, n):
-            if (i, j - 1) in keep:
-                current = keep[(i, j - 1)] + a[j]
-            elif (i - 1, j) in keep:
-                current = keep[(i - 1, j)] - a[i - 1]
-            else:
-                current = sum(a[i:j + 1])
-            keep[(i, j)] = current
-            z = current / (j - i + 1)
-            if z > maxx:
-                maxx = z
+            s += a[j]
+            so_far = s / (j - i + 1)
+            if so_far > maxx:
+                maxx = so_far
     return maxx
 
 print(go())
+
