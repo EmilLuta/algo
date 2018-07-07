86121f5de2187f7047472717c199614ae581dd0b
Devil <virgil.luta@gmail.com>
2018-07-07 11:02:49 +0300

    Fix C for Round 495

--- CodeForces/Round495/c.py
+++ CodeForces/Round495/c.py
@@ -1,5 +1,7 @@
-def go():
-    [int(i) for i in input().split(' ')]
-    return
-
-print(go())
+input()
+c = [0] * 100001
+s = set()
+for x in map(int, input().split(' ')):
+    c[x] = len(s)
+    s.add(x)
+print(sum(c))
