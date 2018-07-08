e35fd78763d37b79e1249ab1b87414b18eb7c0c0
Devil <virgil.luta@gmail.com>
2018-07-08 19:20:04 +0300

    BSet - trees 6

---
+++ LeetCode/BSet/lowest_common_ancestor_binary_tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root in (None, p, q):
+            return root
+        left = self.lowestCommonAncestor(root.left, q, p)
+        right = self.lowestCommonAncestor(root.right, q, p)
+        if left and right:
+            return root
+        else:
+            return left or right
+
+root = TreeNode(3)
+root.left = TreeNode(5)
+root.left.left = TreeNode(6)
+root.left.right = TreeNode(2)
+root.left.right.left = TreeNode(7)
