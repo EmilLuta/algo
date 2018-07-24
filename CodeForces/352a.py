56cef09da099112b4be941b5cc1429bbd9ae479c
EmilLuta <virgil.luta@gmail.com>
2018-07-24 23:24:43 +0300

    Round 352 - A + minor template tweak

---
+++ CodeForces/352a.py
@@ -0,0 +1,10 @@
+def go():
+
+    n = int(input())
+    a = [int(i) for i in input().split(' ')]
+    if not 0 in a:
+        return -1
+    else:
+        return int('5' * (a.count(5) // 9 * 9) + '0' * c.count(0))
+
+print(go())
--- CodeForces/template.py
+++ CodeForces/template.py
@@ -1,4 +1,5 @@
 def go():
+    n = int(input())
     a = [int(i) for i in input().split(' ')]
     return
 
