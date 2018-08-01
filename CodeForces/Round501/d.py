9e1b1804f4e04676ccbebc8fb3b66fe8bffd9b59
EmilLuta <virgil.luta@gmail.com>
2018-08-01 22:42:11 +0300

    Round 501 - D - copycat

--- CodeForces/Round501/d.py
+++ CodeForces/Round501/d.py
@@ -1,6 +1,21 @@
-def go():
-    n = int(input())
-    a = [int(i) for i in input().split(' ')]
-    return
+def step(cur, x):
+    if(cur - x > 0):
+        return cur - x
+    else:
+        return cur + x
 
-print(go())
+
+
+n, k, s = map(int, input().split())
+cur = 1
+
+if(k > s or k * (n - 1) < s):
+    print('NO')
+else:
+    print('YES')
+    while(k > 0):
+        l = min(n - 1, s - (k - 1))
+        cur = step(cur, l)
+        print(cur, end = ' ')
+        s -= l
+        k -= 1
