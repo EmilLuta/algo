87eea0d7fd952c34d9e5efbd4cb9f956eb4bdb58
EmilLuta <virgil.luta@gmail.com>
2018-08-31 21:10:18 +0300

    Moving Zeros - the smart way

--- LeetCode/FSet/move_zeros.py
+++ LeetCode/FSet/move_zeros.py
@@ -16,4 +16,18 @@ class Solution:
                 nums[i], nums[j] = nums[j], nums[i]
         return
 
+    def moveZeroes(self, nums):
+        n = len(nums)
+        slow = upfront = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                upfront += 1
+            else:
+                if upfront < n:
+                    nums[slow], nums[upfront] = nums[upfront], nums[slow]
+                    slow += 1
+                    upfront += 1
+        for i in range(slow, n):
+            nums[i] = 0
+        return nums
 print(Solution().moveZeroes([0,1,0,3,12]))
