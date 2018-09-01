f4d291637e00db9a5d369f5239f666aed6caece7
EmilLuta <virgil.luta@gmail.com>
2018-09-01 10:09:17 +0300

    Intersection of 2 arrays 2 - decent
    The second one could be computed on iteration, rather than ahead.

---
+++ LeetCode/FSet/intersection_of_two_arrays_2.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+class Solution:
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        a = Counter(nums1)
+        b = Counter(nums2)
+        output = []
+        for i in a:
+            if i in b:
+                output += [i] * min(a[i], b[i])
+        return output
+
+print(Solution().intersect([1, 2, 2, 1], [2, 2]))
+print(Solution().intersect([4, 9, 5], [9, 4, 9, 8, 4]))
