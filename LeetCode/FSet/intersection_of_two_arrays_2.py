1ba7d9ac4622a331f739cad72dc11f8acebcab1d
EmilLuta <virgil.luta@gmail.com>
2018-09-01 10:14:19 +0300

    Somewhat better codewise, still could be spastic

--- LeetCode/FSet/intersection_of_two_arrays_2.py
+++ LeetCode/FSet/intersection_of_two_arrays_2.py
@@ -15,5 +15,14 @@ class Solution:
                 output += [i] * min(a[i], b[i])
         return output
 
+    def intersect(self, nums1, nums2):
+        nums = Counter(nums1)
+        output = []
+        for i in nums2:
+            if i in nums and nums[i] > 0:
+                output.append(i)
+                nums[i] -= 1
+        return output
+
 print(Solution().intersect([1, 2, 2, 1], [2, 2]))
 print(Solution().intersect([4, 9, 5], [9, 4, 9, 8, 4]))
