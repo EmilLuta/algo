0f579733efb7e30ed67402d48b9a1a27f9513101
EmilLuta <virgil.luta@gmail.com>
2018-09-06 21:51:32 +0300

    3rd time lucky

--- CodeForces/Round507/a.py
+++ CodeForces/Round507/a.py
@@ -3,22 +3,20 @@ def go():
     x = [int(i) for i in input().split(' ')]
     total = 0
     for i in range(n // 2):
-        if x[i] == x[n - i - 1] and x[i] != 2:
-            continue
-        elif x[i] == x[n - i - 1] and x[i] == 2:
+        if (x[i] == 1 and x[n - 1 - i] == 0) or (x[i] == 0 and x[n - 1 - i] == 1):
+            return -1
+        elif x[i] == 2 and x[i] == x[n - 1 - i]:
             total += 2 * min(a, b)
         elif x[i] == 2:
             if x[n - i - 1] == 1:
                 total += b
             else:
                 total += a
-        elif x[n - i - 1]:
+        elif x[n - i - 1] == 2:
             if x[i] == 1:
                 total += b
             else:
                 total += a
-        else:
-            return -1
     if n % 2 == 1 and x[n // 2] == 2:
         total += min(a, b)
     return total
