39d568032cdfec6f62e48131e49c278fa606e516
devil <virgil.luta@gmail.com>
2021-07-31 21:48:00 +0100

    add extra information

--- CSES/Problems/DynamicProgramming/coin_combinations_2.cpp
+++ CSES/Problems/DynamicProgramming/coin_combinations_2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 const int MOD = 1e9 + 7;
 
-int dp[1000005][105];
+int dp[105][1000005];
 
 int main() {
     int n, x;
@@ -13,24 +13,23 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
-    /* vector<vector<int>> dp(x + 1, vector<int>(n, 0)); */
     dp[0][0] = 1;
-    for (int s = 0; s <= x; s++) {
-        for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
+        for (int s = 0; s <= x; s++) {
             int target = s + a[i];
             if (target <= x) {
-                dp[target][i] += dp[s][i];
-                if (dp[s][i + 1] >= MOD) {
-                    dp[target][i] -= MOD;
+                dp[i][target] += dp[i][s];
+                if (dp[i][target] >= MOD) {
+                    dp[i][target] -= MOD;
                 }
             }
             if (i + 1 < n) {
-                dp[s][i + 1] += dp[s][i];
-                if (dp[s][i + 1] >= MOD) {
-                    dp[s][i + 1] -= MOD;
+                dp[i + 1][s] += dp[i][s];
+                if (dp[i + 1][s] >= MOD) {
+                    dp[i + 1][s] -= MOD;
                 }
             }
         }
     }
-    printf("%d\n", dp[x][n - 1]);
+    printf("%d\n", dp[n - 1][x]);
 }
